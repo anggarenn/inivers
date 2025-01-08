682abe4c9057a2ef6c27f118d974aac0689ae013
anggarenn <57941599+anggarenn@users.noreply.github.com>
2025-01-08 17:54:50 +0700

    Create start_miner_random.sh

---
+++ start_miner_random.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Buat angka acak 3 digit
+WORKER_NUMBER=$(shuf -i 100-999 -n 1)
+
+# Format nama worker
+WORKER_NAME="Worker${WORKER_NUMBER}"
+
+# Jalankan miner dengan nama worker yang tergenerate
+pm2 start /root/iniminer-linux-x64 --name iniminer --no-daemon -- --pool "stratum+tcp://0x74c1dabf09386bbb94dd1c46ac950ab754b0839b.$WORKER_NAME@pool-core-testnet.inichain.com:32672" --cpu-devices 1
