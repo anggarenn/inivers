9c1c88efc13b8e3ea2d997bea98fbf93bb0efdc0
anggarenn <57941599+anggarenn@users.noreply.github.com>
2025-01-08 18:05:38 +0700

    Update start_miner_random.sh

--- start_miner_random.sh
+++ start_miner_random.sh
@@ -8,3 +8,6 @@ WORKER_NAME="Worker${WORKER_NUMBER}"
 
 # Jalankan miner dengan nama worker yang tergenerate
 pm2 start /root/iniminer-linux-x64 --name iniminer --no-daemon -- --pool "stratum+tcp://0x74c1dabf09386bbb94dd1c46ac950ab754b0839b.$WORKER_NAME@pool-core-testnet.inichain.com:32672" --cpu-devices 1
+pm2 startup
+pm2 save
+pm2 list
